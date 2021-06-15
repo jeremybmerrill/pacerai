fa0967d0d9977e6cd8a2054e19cf3b9d728719aa
Jeremy B Merrill <jeremybmerrill@gmail.com>
2021-06-15 16:59:47 -0400

    escape HTML entities in scraper

--- pacerporcupine/pacerporcupine/rss_scraper_chalice/app.py
+++ pacerporcupine/pacerporcupine/rss_scraper_chalice/app.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from os import environ
 from urllib.parse import urlparse, parse_qs
 from time import mktime
+from html import unescape
 
 import feedparser
 from sqlalchemy import (
@@ -126,68 +127,81 @@ class RSSDocketEntry(Base):
     docket_entry_num = Column(Integer)
 
 
-# Automatically runs every 5 minutes
-@app.schedule("cron(0 8-22/2 ? * MON-FRI *)")
+def scrape_court(court):
+    session = Session()
+    try:
+        feed = feedparser.parse(
+            f"https://ecf.{court}.uscourts.gov/cgi-bin/rss_outside.pl"
+        )
+    except ConnectionRefusedError:
+        return
+    for entry in feed.entries:
+        description = unescape(
+            entry["description"]
+        )  # [~Util - Modify Hearings/Deadlines (Full List)] (<a href="https://ecf.txnd.uscourts.gov/doc1/177114116288?caseid=339097&de_seq_num=155" >46</a>)
+        document_type = description.split("(", 1)[0].strip().lstrip("[").rstrip("]")
+        title = unescape(entry["title"])
+        print(entry)
+        if "(<a" in description:
+            document_link = description.split("(", 1)[1][:-1]  # maybe None
+            document_link_soup = BeautifulSoup(document_link, "html.parser").find_all(
+                "a"
+            )[0]
+            parsed_query_string = parse_qs(urlparse(document_link_soup["href"]).query)
+            docket_entry_seq_num = (
+                parsed_query_string["de_seq_num"][0]
+                if "de_seq_num" in parsed_query_string
+                else None
+            )
+            docket_entry_num = document_link_soup.get_text()
+        else:
+            document_link = None
+            document_link_soup = None
+            docket_entry_seq_num = None
+            docket_entry_num = None
+
+        entry_obj = RSSDocketEntry(
+            title=title,
+            case_number=title.split(" ", 1)[0],
+            case_name=title.split(" ", 1)[1] if " " in title else None,
+            docket_id=entry["link"].split("?")[-1],
+            document_type=document_type,
+            description=description,
+            docket_entry_seq_num=docket_entry_seq_num,
+            docket_entry_num=docket_entry_num,
+            guid=entry["guid"],
+            pub_date=datetime.fromtimestamp(mktime(entry.published_parsed)),
+            scrape_date=datetime.now(),
+            court=court,
+        )
+        session.merge(entry_obj)
+    session.commit()
+
+
+# Automatically runs every two hours minutes
+# @app.schedule("cron(0 8-22/2 ? * MON-FRI *)")
 def scrape_all_courts(event):
     # visit each one's RSS feed
     Base.metadata.create_all(engine)
 
-    session = Session()
     for court in courts:
-        try:
-            feed = feedparser.parse(
-                f"https://ecf.{court}.uscourts.gov/cgi-bin/rss_outside.pl"
-            )
-        except ConnectionRefusedError:
-            continue
-        for entry in feed.entries:
-            description = entry[
-                "description"
-            ]  # [~Util - Modify Hearings/Deadlines (Full List)] (<a href="https://ecf.txnd.uscourts.gov/doc1/177114116288?caseid=339097&de_seq_num=155" >46</a>)
-            document_type = description.split("(", 1)[0].strip().lstrip("[").rstrip("]")
-            print(entry)
-            if "(<a" in description:
-                document_link = description.split("(", 1)[1][:-1]  # maybe None
-                document_link_soup = BeautifulSoup(
-                    document_link, "html.parser"
-                ).find_all("a")[0]
-                parsed_query_string = parse_qs(
-                    urlparse(document_link_soup["href"]).query
-                )
-                docket_entry_seq_num = (
-                    parsed_query_string["de_seq_num"][0]
-                    if "de_seq_num" in parsed_query_string
-                    else None
-                )
-                docket_entry_num = document_link_soup.get_text()
-            else:
-                document_link = None
-                document_link_soup = None
-                docket_entry_seq_num = None
-                docket_entry_num = None
+        scrape_court(court)
 
-            entry_obj = RSSDocketEntry(
-                title=entry["title"],
-                case_number=entry["title"].split(" ", 1)[0],
-                case_name=entry["title"].split(" ", 1)[1]
-                if " " in entry["title"]
-                else None,
-                docket_id=entry["link"].split("?")[-1],
-                document_type=document_type,
-                description=description,
-                docket_entry_seq_num=docket_entry_seq_num,
-                docket_entry_num=docket_entry_num,
-                guid=entry["guid"],
-                pub_date=datetime.fromtimestamp(mktime(entry.published_parsed)),
-                scrape_date=datetime.now(),
-                court=court,
-            )
-            session.merge(entry_obj)
-        session.commit()
 
+for i, court in enumerate(courts):
+    exec(
+        """
+@app.schedule("cron({} 8-22/2 ? * MON-FRI *)")
+def scrape_{}(event):
+    scrape_court("{}")
+
+    """.format(
+            int(i / 2), court, court
+        )
+    )
 
 if __name__ == "__main__":
-    scrape_all_courts(
+    scrape_ohnd(
         {
             "version": "0",
             "id": "53dc4d37-cffa-4f76-80c9-8b7d4a4d2eaa",
@@ -200,7 +214,8 @@ if __name__ == "__main__":
                 "arn:aws:events:us-east-1:123456789012:rule/my-scheduled-rule"
             ],
             "detail": {},
-        }
+        },
+        {},
     )
 
 #
