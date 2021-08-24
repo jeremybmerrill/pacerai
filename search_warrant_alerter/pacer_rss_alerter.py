ccb64fda8d0815abe7132bef2c18a20bd303f295
Jeremy B Merrill <jeremybmerrill@gmail.com>
2021-08-24 18:38:25 -0400

    fix forgotten imports

--- search_warrant_alerter/pacer_rss_alerter.py
+++ search_warrant_alerter/pacer_rss_alerter.py
@@ -1,7 +1,13 @@
+from os import environ
+
+from sqlalchemy import create_engine, or_
+
 from pacerporcupine.pacer_rss_search_warrant_alerter.alert import (
     alert_based_on_pacer_rss,
 )
 from pacerporcupine.db import Base
+
+ENGINE = create_engine(environ.get("LIVE_DATABASE_URL"))
 
 
 def handler(event, context):
