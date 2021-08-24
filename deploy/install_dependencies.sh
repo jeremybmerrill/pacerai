9e4e8fa37b73d7cb9356a120ca8e607bf63acae5
Jeremy B Merrill <jeremybmerrill@gmail.com>
2021-08-24 18:38:42 -0400

    fix deploy

--- deploy/install_dependencies.sh
+++ deploy/install_dependencies.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-source /home/ubuntu/tow-pacer/infra/deploy/install_root_dependencies.sh
+source /home/ubuntu/tow-pacer/deploy/install_root_dependencies.sh
 
-sudo chmod 777 -R /home/ubuntu/tow-pacer/infra/deploy/
-cd /home/ubuntu/tow-pacer/infra/deploy/
+sudo chmod 777 -R /home/ubuntu/tow-pacer/deploy/
+cd /home/ubuntu/tow-pacer/deploy/
 
 # setup services
-ls /home/ubuntu/tow-pacer/infra/deploy/services | xargs -i sudo install -m u+rw,ugo-x,go-w,go+r services/"{}" "/lib/systemd/system/{}"
+ls /home/ubuntu/tow-pacer/deploy/services | xargs -i sudo install -m u+rw,ugo-x,go-w,go+r services/"{}" "/lib/systemd/system/{}"
 sudo systemctl daemon-reload
 sudo systemctl enable courtlistener_search_warrant_alerter.service
 sudo systemctl start courtlistener_search_warrant_alerter.service
@@ -17,12 +17,12 @@ sudo systemctl start rss_search_warrant_alerter.timer
 
 
 # setup server
-cd /home/ubuntu/tow-pacer/infra/deploy/
+cd /home/ubuntu/tow-pacer/deploy/
 
 # pull secrets
 export $(cat /home/ubuntu/.env | xargs)
 
-sudo chmod 777 -R /home/ubuntu/tow-pacer/infra/deploy/
+sudo chmod 777 -R /home/ubuntu/tow-pacer/deploy/
 pip3 install -U --user pip # default pip causes some problems
 pip3 install --user  --no-cache-dir -r /home/ubuntu/tow-pacer/search_warrant_alerter/requirements.txt # no-cache-dir causes some problems
 rm -rf /tmp/pacerporcupine/
